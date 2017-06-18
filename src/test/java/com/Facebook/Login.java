f19f953dfbe6506ae4da2eeb0671ba7a7f00db27
veera <prathap.ufttest@gmail.com>
2017-06-18 08:51:58 +0530

    i wrote a new code for browse launching

---
+++ src/test/java/com/Facebook/Login.java
@@ -0,0 +1,23 @@
+package com.Facebook;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Login {
+	
+	
+	@Test
+	public static void launchBrowser()
+	{
+		WebDriver driver=new FirefoxDriver();
+		
+		driver.navigate().to("https://www.facebook.com/");
+		
+		driver.manage().window().maximize();
+		
+		
+		
+	}
+
+}
